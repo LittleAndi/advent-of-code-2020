da11327535478a09b7e5fd85dec0116f1776ab82
Lars Nyström <lars.nystrom@mio.se>
2020-12-01 07:32:38 +0100

    Refactor main for part two

--- day01/Program.cs
+++ day01/Program.cs
@@ -13,14 +13,26 @@ namespace day01
                 .Select(l => int.Parse(l))
                 .ToArray<int>();
 
+            PartOne(lines);
+
+            PartTwo(lines);
+        }
+
+        private static void PartTwo(int[] lines)
+        {
+            throw new NotImplementedException();
+        }
+
+        private static void PartOne(int[] lines)
+        {
             for (int i = 0; i < lines.Length; i++)
             {
-                for (int j = i+1; j < lines.Length; j++)
+                for (int j = i + 1; j < lines.Length; j++)
                 {
-                    if (lines[i]+lines[j] == 2020)
+                    if (lines[i] + lines[j] == 2020)
                     {
-                        Console.WriteLine($"{lines[i]} + {lines[j]} = {lines[i]+lines[j]}");
-                        Console.WriteLine($"{lines[i]} * {lines[j]} = {lines[i]*lines[j]}");
+                        Console.WriteLine($"{lines[i]} + {lines[j]} = {lines[i] + lines[j]}");
+                        Console.WriteLine($"{lines[i]} * {lines[j]} = {lines[i] * lines[j]}");
                         return;
                     }
                 }

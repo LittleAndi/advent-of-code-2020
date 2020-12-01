56551feda70df51efb7ddc9a3cc50fbbd64b30c4
Lars Nyström <lars.nystrom@mio.se>
2020-12-01 07:31:15 +0100

    Solve part one

--- day01/Program.cs
+++ day01/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.IO;
+using System.Linq;
 
 namespace day01
 {
@@ -6,7 +8,23 @@ namespace day01
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var lines = File.ReadAllLines("input.txt")
+                .Where(l => !string.IsNullOrWhiteSpace(l))
+                .Select(l => int.Parse(l))
+                .ToArray<int>();
+
+            for (int i = 0; i < lines.Length; i++)
+            {
+                for (int j = i+1; j < lines.Length; j++)
+                {
+                    if (lines[i]+lines[j] == 2020)
+                    {
+                        Console.WriteLine($"{lines[i]} + {lines[j]} = {lines[i]+lines[j]}");
+                        Console.WriteLine($"{lines[i]} * {lines[j]} = {lines[i]*lines[j]}");
+                        return;
+                    }
+                }
+            }
         }
     }
 }

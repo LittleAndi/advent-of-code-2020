712bc86b3a3b9d6007e5725577a47092742ab720
Lars Nyström <nystrom.lasse@gmail.com>
2020-12-01 07:55:22 +0100

    Update new day script

--- newday.cmd
+++ newday.cmd
@@ -1,9 +1,12 @@
 dotnet new console -n %1
+dotnet sln add %1/%1.csproj
 cd tests
 dotnet new xunit -n %1tests
+dotnet sln ../advent-of-code-2020.sln add %1tests/%1tests.csproj
 cd %1tests
 dotnet add reference ../../%1/%1.csproj
 dotnet add package Shouldly
 dotnet restore
+cd ../..
 git add .
 git commit -m "Add %1"

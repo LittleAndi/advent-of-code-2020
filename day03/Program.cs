13ef56f3554578b8eb2e68b9a9ca9f048f98519a
Lars Nyström <nystrom.lasse@gmail.com>
2020-12-04 00:58:59 +0100

    Solution for part one

--- day03/Program.cs
+++ day03/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.IO;
+using System.Linq;
 
 namespace day03
 {
@@ -6,7 +8,26 @@ namespace day03
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var lines = File.ReadAllLines("input.txt")
+                .Where(l => !string.IsNullOrWhiteSpace(l))
+                .ToArray();
+
+            var lineLength = lines.First().Length;
+            var x = 0 + 3;
+            var y = 0 + 1;
+            var trees = 0;
+
+            do
+            {
+                if (lines[y][x].Equals('#')) trees++;
+
+                x += 3;
+                y += 1;
+
+                if (x > lineLength - 1) x -= lineLength;
+            } while (y < lines.Count());
+
+            System.Console.WriteLine($"{trees} trees encounterd");
         }
     }
 }

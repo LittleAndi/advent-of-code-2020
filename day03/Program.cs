c2cf7935ba53ee0e7ebf86dcf72e6b39819afc5f
Lars Nyström <nystrom.lasse@gmail.com>
2020-12-04 01:28:58 +0100

    Day03 (#2)
    * Add day03
    
    * Update vscode files
    
    * Rename class
    
    * Add input
    
    * Solution for part one
    
    * Refactor for part two
    
    * Add solution for part two

---
+++ day03/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace day03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var lines = File.ReadAllLines("input.txt")
+                .Where(l => !string.IsNullOrWhiteSpace(l))
+                .ToArray();
+
+            var trees = GetTreesEncounterd(lines, 3, 1);
+
+            System.Console.WriteLine($"Part one: {trees} trees encounterd");
+
+            var encounterdTreesProduct = 1;
+            encounterdTreesProduct *= GetTreesEncounterd(lines, 1, 1);
+            encounterdTreesProduct *= GetTreesEncounterd(lines, 3, 1);
+            encounterdTreesProduct *= GetTreesEncounterd(lines, 5, 1);
+            encounterdTreesProduct *= GetTreesEncounterd(lines, 7, 1);
+            encounterdTreesProduct *= GetTreesEncounterd(lines, 1, 2);
+
+            System.Console.WriteLine($"Part two: Product of trees encounterd {encounterdTreesProduct}");
+        }
+
+        static int GetTreesEncounterd(string[] lines, int movX, int movY)
+        {
+            var lineLength = lines.First().Length;
+            var x = movX;
+            var y = movY;
+            var trees = 0;
+
+            do
+            {
+                if (lines[y][x].Equals('#')) trees++;
+
+                x += movX;
+                y += movY;
+
+                if (x > lineLength - 1) x -= lineLength;
+            } while (y < lines.Count());
+
+            return trees;
+        }
+    }
+}
---
+++ tests/day03tests/Day03Tests.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace day03tests
+{
+    public class Day03Tests
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}

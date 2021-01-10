ed412bf93559115a666d6180113a3c74178a3f39
Lars Nyström <nystrom.lasse@gmail.com>
2021-01-10 14:37:21 +0100

    Read first part of input

--- day16/Program.cs
+++ day16/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.IO;
+using System.Text.RegularExpressions;
 
 namespace day16
 {
@@ -6,7 +8,24 @@ namespace day16
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var lines = File.ReadAllLines("input.txt");
+
+            Regex regexField = new Regex(@"(?<field>[a-z ]+): (?<start1>\d+)-(?<end1>\d+) or (?<start2>\d+)-(?<end2>\d+)");
+
+            var ticketValidator = new TicketValidator();
+            var line = 0;
+
+            Match fieldMatch;
+            while ((fieldMatch = regexField.Match(lines[line])).Success)
+            {
+                ticketValidator.AddRule(
+                    fieldMatch.Groups["field"].Value,
+                    int.Parse(fieldMatch.Groups["start1"].Value),
+                    int.Parse(fieldMatch.Groups["end1"].Value),
+                    int.Parse(fieldMatch.Groups["start2"].Value),
+                    int.Parse(fieldMatch.Groups["end2"].Value));
+                line++;
+            }
         }
     }
 

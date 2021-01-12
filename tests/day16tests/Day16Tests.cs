42ea57083df0409de05a908a7df639b453a4ac3f
Lars Nyström <nystrom.lasse@gmail.com>
2021-01-12 01:33:40 +0100

    Test predicate builder

--- tests/day16tests/Day16Tests.cs
+++ tests/day16tests/Day16Tests.cs
@@ -1,4 +1,6 @@
 using System;
+using System.Collections.Generic;
+using System.Linq;
 using day16;
 using Shouldly;
 using Xunit;
@@ -19,6 +21,18 @@ namespace day16tests
             ticketValidator.AddRule("row", 6, 11, 33, 44);
             ticketValidator.AddRule("seat", 13, 40, 45, 50);
             ticketValidator.IsTicketValid(ticketInfo).ShouldBe(result);
+        }
+
+        [Fact]
+        public void TestPredicted()
+        {
+            var predicate = PredicateBuilder.False<int>();
+            predicate = predicate.Or(i => i >= 1 && i <= 3);
+            predicate = predicate.Or(i => i >= 5 && i <= 6);
+
+            var numbers = new List<int> { 1, 2, 3, 4, 5, 6, 7 };
+            var filteredNumbes = numbers.AsQueryable().Where(predicate);
+            filteredNumbes.ShouldBe(new List<int> { 1, 2, 3, 5, 6 });
         }
     }
 }
